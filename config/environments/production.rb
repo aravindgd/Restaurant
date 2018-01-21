7997189415aa9ec535ae99064b2f9bc9cc6bc7d5
swapnilb-009 <swap.b009@gmail.com>
2018-01-21 18:09:34 +0530

    smptp configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
