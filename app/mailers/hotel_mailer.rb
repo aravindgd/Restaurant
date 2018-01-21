0f4d96ac6b3f4fe622cf98233853419652c332c9
swapnilb-009 <swap.b009@gmail.com>
2018-01-21 17:33:26 +0530

    mailer added

--- app/mailers/hotel_mailer.rb
+++ app/mailers/hotel_mailer.rb
@@ -8,7 +8,6 @@ class HotelMailer <  ActionMailer::Base
  
   def updated_guest_details(guest, reservation_details)
     @user = guest
-    @reservation_details = reservation_details
     #@password = password
     mail(to: 'swapnil.m.bhosale@gmail.com', subject: 'Reservation Successfull')
   end
