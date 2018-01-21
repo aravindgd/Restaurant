df5f7e8806cc2bd01b083c3f2abd432130acec98
swapnilb-009 <swap.b009@gmail.com>
2018-01-21 18:03:15 +0530

    production.rb changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "swap.b009@gmail.com",
+    :user_name            => "swap.b009@info.com",
     :password             => "Abc@12345",
     :authentication       => "plain",
     :enable_starttls_auto => true
